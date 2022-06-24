422caca1612140c75c885ace870e5dca1b139152
Andrei-Stefan BURSUC <abursuc@pentalog.com>
2022-06-24 10:59:51 +0300

    Implement domain layer

---
+++ DailyMotion/DailyMotion/Domain/UseCases/GetVideosUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  GetVideosUseCase.swift
+//  DailyMotion
+//
+//  Created by Andrei-Stefan BURSUC on 24.06.2022.
+//
+
+import Foundation
+
+protocol GetVideosUseCaseProtocol {
+    var videoRepository: VideoRepositoryProtocol { get }
+
+    /// Retrieve the list of videos
+    func retrieveVideos(then success: @escaping SuccessCompletionHandler, error: @escaping ErrorCompletionHandler)
+}
+
+struct GetVideosUseCase: GetVideosUseCaseProtocol {
+
+    // MARK: - Parameters
+
+    let videoRepository: VideoRepositoryProtocol
+
+    // MARK: - Initializer
+
+    init(videoRepository: VideoRepositoryProtocol = VideoRepository()) {
+        self.videoRepository = videoRepository
+    }
+
+    // MARK: - GetVideosUseCaseProtocol
+
+    func retrieveVideos(then success: @escaping SuccessCompletionHandler, error: @escaping ErrorCompletionHandler) {
+        videoRepository.videos(then: success, error: error)
+    }
+}
